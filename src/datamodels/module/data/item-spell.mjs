c27bffb586224fabb0985d277030d70aee09f04e
Nuptup <66802180+NuptupTDOW@users.noreply.github.com>
2024-08-26 11:30:44 -0500

    Update item-spell.mjs

--- src/datamodels/module/data/item-spell.mjs
+++ src/datamodels/module/data/item-spell.mjs
@@ -1,6 +1,6 @@
-import BoilerplateItemBase from "./base-item.mjs";
+import BuckLegacyItemBase from "./base-item.mjs";
 
-export default class BoilerplateSpell extends BoilerplateItemBase {
+export default class BuckLegacySpell extends BuckLegacyItemBase {
 
   static defineSchema() {
     const fields = foundry.data.fields;
