fb50d957cee2632dadf2ccca8c1ce2f5943e40c0
Nuptup <66802180+NuptupTDOW@users.noreply.github.com>
2024-08-26 11:45:31 -0500

    Update config.mjs

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,23 @@
-export const BOILERPLATE = {};
+export const BUCKLEGACY = {};
 
 /**
  * The set of Ability Scores used within the system.
  * @type {Object}
  */
-BOILERPLATE.abilities = {
-  str: 'BOILERPLATE.Ability.Str.long',
-  dex: 'BOILERPLATE.Ability.Dex.long',
-  con: 'BOILERPLATE.Ability.Con.long',
-  int: 'BOILERPLATE.Ability.Int.long',
-  wis: 'BOILERPLATE.Ability.Wis.long',
-  cha: 'BOILERPLATE.Ability.Cha.long',
+BUCKLEGACY.abilities = {
+  str: 'BUCKLEGACY.Ability.Str.long',
+  dex: 'BUCKLEGACY.Ability.Dex.long',
+  con: 'BUCKLEGACY.Ability.Con.long',
+  int: 'BUCKLEGACY.Ability.Int.long',
+  wis: 'BUCKLEGACY.Ability.Wis.long',
+  cha: 'BUCKLEGACY.Ability.Cha.long',
 };
 
-BOILERPLATE.abilityAbbreviations = {
-  str: 'BOILERPLATE.Ability.Str.abbr',
-  dex: 'BOILERPLATE.Ability.Dex.abbr',
-  con: 'BOILERPLATE.Ability.Con.abbr',
-  int: 'BOILERPLATE.Ability.Int.abbr',
-  wis: 'BOILERPLATE.Ability.Wis.abbr',
-  cha: 'BOILERPLATE.Ability.Cha.abbr',
+BUCKLEGACY.abilityAbbreviations = {
+  str: 'BUCKLEGACY.Ability.Str.abbr',
+  dex: 'BUCKLEGACY.Ability.Dex.abbr',
+  con: 'BUCKLEGACY.Ability.Con.abbr',
+  int: 'BUCKLEGACY.Ability.Int.abbr',
+  wis: 'BUCKLEGACY.Ability.Wis.abbr',
+  cha: 'BUCKLEGACY.Ability.Cha.abbr',
 };
