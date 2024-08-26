986aab96308352a5a5a73d57c330d435b5368170
Nuptup <66802180+NuptupTDOW@users.noreply.github.com>
2024-08-26 11:26:34 -0500

    Update generate-boilerplate-system.mjs

--- src/generate-boilerplate-system.mjs
+++ src/generate-boilerplate-system.mjs
@@ -102,37 +102,37 @@ class SystemGenerator {
   /**
    * Replace file contents.
    *
-   * Replace refercnes to 'boilerplate', 'Boilerplate', and 'BOILERPLATE'
+   * Replace refercnes to 'buck-legacy', 'BuckLegacy', and 'BUCKLEGACY'
    * in files copied over to the build directory.
    */
   replaceFileContents() {
     // Set patterns to iterate over later.
     const replacements = [
       {
-        pattern: new RegExp(/game\.boilerplate/g),
+        pattern: new RegExp(/game\.buck-legacy/g),
         replacement: `game.${this.propName}`
       },
       {
-        pattern: new RegExp(/flags\.boilerplate/g),
+        pattern: new RegExp(/flags\.buck-legacy/g),
         replacement: `flags.${this.propName}`
       },
       {
-        pattern: 'boilerplate',
+        pattern: 'buck-legacy',
         replacement: this.packageName
       },
       {
-        pattern: 'Boilerplate',
+        pattern: 'BuckLegacy',
         replacement: this.className
       },
       {
-        pattern: 'BOILERPLATE',
+        pattern: 'BUCKLEGACY',
         replacement: this.constantName
       }
     ];
 
     // Update title in system.json.
     replace({
-      regex: 'Boilerplate',
+      regex: 'BuckLegacy',
       replacement: this.titleName,
       paths: [`./build/${this.packageName}/system.json`],
       silent: true
@@ -158,15 +158,15 @@ class SystemGenerator {
   /**
    * Rename files.
    *
-   * Rename files that had boilerplate in their name, such as
-   * css/boilerplate.css.
+   * Rename files that had buck-legacy in their name, such as
+   * css/buck-legacy.css.
    */
   renameFiles() {
-    glob(`build/${this.packageName}/**/*boilerplate*.*`).then(files => {
+    glob(`build/${this.packageName}/**/*buck-legacy*.*`).then(files => {
       files.forEach(oldPath => {
         const file = path.basename(oldPath);
         const directory = path.dirname(oldPath);
-        fs.rename(oldPath, `${directory}/${file.replaceAll('boilerplate', this.packageName)}`, (err) => {
+        fs.rename(oldPath, `${directory}/${file.replaceAll('buck-legacy', this.packageName)}`, (err) => {
           if (err) throw err;
         });
       })
@@ -183,7 +183,7 @@ class SystemGenerator {
    */
   cleanPackageJson() {
     // Remove unneeded files.
-    fs.rmSync(`build/${this.packageName}/src/generate-boilerplate-system.mjs`);
+    fs.rmSync(`build/${this.packageName}/src/generate-buck-legacy-system.mjs`);
     fs.rmSync(`build/${this.packageName}/package-lock.json`);
 
     // Load package.json so that we can remove dev dependencies.
@@ -254,13 +254,13 @@ inquirer
     // Clean out our build directory.
     generator.cleanBuildDir();
 
-    // Glob Boilerplate's files so that we can process them.
+    // Glob BuckLegacy's files so that we can process them.
     glob('*', {ignore: ['node_modules/**'] }).then(files => {
       // Copy all files into the build dir.
       generator.copyFiles(files);
-      // Replace boilerplate name mentions in files.
+      // Replace buck-legacy name mentions in files.
       generator.replaceFileContents();
-      // Rename files that had boilerplate in their name.
+      // Rename files that had buck-legacy in their name.
       generator.renameFiles();
       // Remove generator files and update package.json.
       generator.cleanPackageJson();
