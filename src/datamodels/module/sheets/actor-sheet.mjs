90eb448dbbe272e5de2dcabfa2a8af99d9cb7f02
Nuptup <66802180+NuptupTDOW@users.noreply.github.com>
2024-08-26 11:29:49 -0500

    Update actor-sheet.mjs

--- src/datamodels/module/sheets/actor-sheet.mjs
+++ src/datamodels/module/sheets/actor-sheet.mjs
@@ -7,11 +7,11 @@ import {
  * Extend the basic ActorSheet with some very simple modifications
  * @extends {ActorSheet}
  */
-export class BoilerplateActorSheet extends ActorSheet {
+export class BuckLegacyActorSheet extends ActorSheet {
   /** @override */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ['boilerplate', 'sheet', 'actor'],
+      classes: ['buck-legacy', 'sheet', 'actor'],
       width: 600,
       height: 600,
       tabs: [
@@ -26,7 +26,7 @@ export class BoilerplateActorSheet extends ActorSheet {
 
   /** @override */
   get template() {
-    return `systems/boilerplate/templates/actor/actor-${this.actor.type}-sheet.hbs`;
+    return `systems/buck-legacy/templates/actor/actor-${this.actor.type}-sheet.hbs`;
   }
 
   /* -------------------------------------------- */
@@ -46,8 +46,8 @@ export class BoilerplateActorSheet extends ActorSheet {
     context.system = actorData.system;
     context.flags = actorData.flags;
 
-    // Adding a pointer to CONFIG.BOILERPLATE
-    context.config = CONFIG.BOILERPLATE;
+    // Adding a pointer to CONFIG.BUCKLEGACY
+    context.config = CONFIG.BUCKLEGACY;
 
     // Prepare character data and items.
     if (actorData.type == 'character') {
